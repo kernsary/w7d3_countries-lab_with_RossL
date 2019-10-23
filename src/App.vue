a5914cb97cb73dd146528dc272ce010fe06d76f1
Ross Lorimer <wross.lorimer@googlemail.com>
2019-10-23 14:26:22 +0100

    countries list displayed

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,35 @@
-<template lang="html">
+<template>
+  <div>
+    <h1>Countries</h1>
+    <div>
+      <countries-list :countries="countries"></countries-list>
+
+    </div>
+  </div>
 
 </template>
 
 <script>
+import CountriesList from "./components/CountriesList.vue";
+
+
 export default {
   name: "app",
   data(){
     return{
-    countries: [],
-    selectedCountry: null
-  };
+      countries: [],
+      selectedCountry: null
+    };
 
-},
-mounted(){
-  fetch("https://restcountries.eu/rest/v2/all")
-  .then(response => response.json())
-  .then(countries => this.countries = countries)
-}
+  },
+  mounted(){
+    fetch("https://restcountries.eu/rest/v2/all")
+    .then(response => response.json())
+    .then(countries => this.countries = countries)
+  },
+  components: {
+    "countries-list": CountriesList
+  }
 }
 </script>
 
