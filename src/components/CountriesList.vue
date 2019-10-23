ed928db047981821f88d6f85171da8a8bc795ab8
Charles Kennedy <charlesscottkennedy@googlemail.com>
2019-10-23 22:51:12 +0100

    added search bar

--- src/components/CountriesList.vue
+++ src/components/CountriesList.vue
@@ -1,9 +1,13 @@
 <template lang="html">
   <div>
-    <label for="country-select">Select a Country</label>
-    <select id="country-select" @change="handleChange" v-model="selectedCountry">
-      <option v-for="country in countries" :value="country">{{country.name}}</option>
-    </select>
+    <form v-on:submit.prevent>
+      <input type="text" placeholder="Search for a country ..." v-on:keyup="handleSearch" v-model="search">
+      <!-- <label for="country-select">Select a Country</label> -->
+      <select id="country-select" @change="handleChange" v-model="selectedCountry">
+        <option disabled value="">Select a country</option>
+        <option v-for="country in countries" :value="country">{{country.name}}</option>
+      </select>
+    </form>
     <!-- <ul>
     <list-item v-for="(country, index) in countries"
     :country="country"
@@ -14,26 +18,36 @@
 </template>
 
 <script>
-import ListItem from "./ListItem.vue";
+// import ListItem from "./ListItem.vue";
 import {eventBus} from "../main.js"
 
 export default {
   name: "countries-list",
   data(){
     return{
+      search: "",
       selectedCountry: null
     }
   },
   props: ["countries"],
   methods: {
+    handleSearch(){
+      let foundCountry = this.countries.find((country) => {
+        return country.name.toLowerCase().indexOf(this.search.toLowerCase()) > -1
+      })
+      this.selectedCountry = foundCountry;
+      eventBus.$emit('country-selected', this.selectedCountry);
+    },
+
     handleChange(){
       eventBus.$emit('country-selected', this.selectedCountry);
     }
-  },
-  components: {
-    "list-item": ListItem
-  }
+    // ,
+    // components: {
+    //   "list-item": ListItem
+    // }
 
+  }
 }
 </script>
 
